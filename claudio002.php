7836ea6497e2a2dc09e9a1b488da8ce054919281
Nathan Trujillo <kelvin.beats@gmail.com>
2013-08-10 01:10:45 -0700

    Update!!

--- claudio002.php
+++ claudio002.php
@@ -1,102 +1,142 @@
 <?php
 
-parsefileIntoMemory();
 
 
 
-if(isset($argv[1])) {
-	if ($handle = opendir($argv[1])) {
-	    echo "Directory handle: $handle\n";
-	    echo "Entries:\n";
+
+
+class claudio {
+
+	public $dataStore = array();
+
 	
-	    /* This is the correct way to loop over the directory. */
-	    while (false !== ($entry = readdir($handle))) {
-	    	if(preg_match("!(\.xls|\.csv)$!i", $entry)){
-		    	print "{$entry}\n";	
-		    	$dest = "{$argv[1]}/" 
-		    		. preg_replace("!\.!", ".new.", $entry);
-		    	$src = "{$argv[1]}/{$entry}";
-		    	process($src,$dest);
-	    	}
+	public function __construct()
+	{
+		global $argv;
+		
+		$this->parsefileIntoMemory();
+		if(isset($argv[1])) {
+			if ($handle = opendir($argv[1])) {
+				echo "Entries:\n";
+			
+			    /* This is the correct way to loop over the directory. */
+			    while (false !== ($entry = readdir($handle))) {
+			    	if(preg_match("!(\.xls|\.csv)$!i", $entry, $matches)){
+				    	print "{$entry}\n";	
+				    	$dest = "{$argv[1]}/" 
+				    		. preg_replace("!" .  $matches[1] ."!", ".out", $entry);
+				    	$src = "{$argv[1]}/{$entry}";
+				    	$this->process($src,$dest);
+			    	}
+			    }
+			}
+		}
+	}
+
+
+
+
+	public function process($in, $out)
+	{
+		print "Working on {$in} --> {$out}\n";
+		
+		$fp = fopen($in, "rw");
+		$newFp = fopen($out, "w");
+	
+		$patternOne = "(^K\.|K\.[A-Z]$|^K\.[[\w\[\]\.]]+K\.[A-Z]$)";
+	
+		$tally = array();
+	
+		$inc = 0;
+	
+		while (($data = fgetcsv($fp, 1000, "\t")) !== FALSE) {
+	
+			$inc++;
+			$matchPatternOne = 0;
+			$position = null;			
+			
+			list($a,$b,$c,$d,$e,$f,$g) = $data;
+			
+			if(array_key_exists($e, $tally)){
+				continue;
+			}
+	
+			$tally[$e] = 1;
+	
+			if(preg_match_all("!{$patternOne}!", $e, $matches)){
+				
+				
+				print_r($matches);
+
+				
+				$matchPatternOne = 1;
+				$lookup = array();
+
+				if(preg_match("!\.([\w\[\]\.]+)\.!i", $e, $lookup)) {
+					//print "{$e} Matches!!\n";
+//					print " --->> " . print_r($lookup[1], true) . "\n";				/
+//					print "Looking up:\n";
+					
+					//look up in database and find position of k.xxxxxx					
+					$position = $this->lookupPartial($f, preg_replace("![\.\[\]0-9]!", "", $lookup[1]));
+				}
+							
+				
+				
+			}
+			
+			if ($matchPatternOne) {
+				$data[] = $matchPatternOne;
+				$data[] = $position;
+				
+				$writeOut = join("\t", $data) . "\n";
+				
+				fwrite($newFp, $writeOut);
+			}	
+		}
+	}
+
+
+
+	public function lookupPartial($key, $e) {
+			
+			print "\t\tSEARCH::{$e}\n";
+			
+			$found = array();
+			if (preg_match("!($e)!im", $this->dataStore[$key], $found, PREG_OFFSET_CAPTURE)) {
+				//print "bammmm\n";
+				//print_r($found);
+				return $found[1][1];
+			}
+	}
+
+
+
+
+
+
+
+
+	public function parsefileIntoMemory(){
+		
+	  $fp = fopen("Yeast.RS.fasta", "r");
+	  
+	  while(($line = fgets($fp, 4096)) !== false){  	
+	  	if(preg_match("!^>!", $line)) { //new entry	  		
+	  		//print "{$line}";
+	  		list($sysName, $geneName) = explode(" ", $line);
+	  		$sysName = preg_replace("!^>!", "", $sysName);
+	  		$this->dataStore[$sysName] = "";  		  		
+	  	}	  		
+	    if(!preg_match("!^>!", $line)){
+	  		$this->dataStore[$sysName] .= trim($line);
 	    }
+	  }
 	}
 }
 
 
-
-function process($in,$out)
-{
-
-	$fp = fopen($in, "rw");
-	$newFp = fopen($out, "w");
-
-	$patternOne = "(^K\.|K\.$|K[^\.])";
-	///false cases
-	$patternTwo = "(\..+(K[^\[]).+\.)";
-	//true case COUNT THESE
-	$patternThree = "(K\[170\.11\])";
-	$patternFour = "K\[170\.11\]\.";
-
-
-	$tally = array();
-
-	$inc = 0;
-
-	while (($data = fgetcsv($fp, 1000, "\t")) !== FALSE) {
-
-		$inc++;
-
-		list($a,$b,$c,$d,$e,$f,$g,$h,$i,$j,$k) = $data;
-
-		if(array_key_exists($e, $tally)){
-			print "hhhhiiii {$e}";
-			continue;
-		}
-
-		$tally[$e] = 1;
-
-		if(preg_match("!{$patternOne}!", $e, $matches, PREG_OFFSET_CAPTURE)){
-			$h = 1;
-			//look up in database and find position of k.xxxxxx
-			
-			
-		}
-
-		fwrite($newFp, "$a\t$b\t$c\t$d\t$e\t$f\t$g\t$h\t$i\t$j\t$k\n");
-
-	}
-}
-
-
-
-function parsefileIntoMemory(){
-	
-  $fp = fopen("/Users/nathantrujillo/Downloads/Yeast.RS.fasta", "r");
-  $dataStore = array();
-  
-  while(($line = fgets($fp, 4096)) !== false){  	
-  	if(preg_match("!^>!", $line)) { //new entry	  		
-  		print "{$line}";
-  		list($sysName, $geneName) = explode(" ", $line);
-  		$sysName = preg_replace("!^>!", "", $sysName);
-  		$dataStore[$sysName] = "";  		  		
-  	}	  		
-    if(!preg_match("!^>!", $line)){
-  		$dataStore[$sysName] .= trim($line);
-    }
-  }
-  print_r($dataStore);
-}
-
-
-
-function findPatternInFile($pattern, $orf = null){
-	
-	
-	
-	
-}
-
+$clajda = new claudio();
 
 
 
